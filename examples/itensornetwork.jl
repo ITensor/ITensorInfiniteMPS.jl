8600389675e443efa9629dd7a943309e5eebda85
Matthew Fishman <mfishman@flatironinstitute.org>
2021-02-26 17:11:02 -0500

    Add some more benchmark results

--- examples/itensornetwork.jl
+++ examples/itensornetwork.jl
@@ -10,61 +10,102 @@ using StatsBase # For sample
 # Testing for improved algorithms
 using ITensorsInfiniteMPS.ContractionSequenceOptimization
 
-Random.seed!(1234)
+function main(N; enable_caching = false)
+  Random.seed!(1234)
 
-N = 2 # Tensors in the network
+  Sz0 = ("Sz", 0) => 1
+  Sz1 = ("Sz", 1) => 1
+  #space = Sz0 ⊕ Sz1
+  space = 2
 
-Sz0 = ("Sz", 0) => 1
-Sz1 = ("Sz", 1) => 1
-#space = Sz0 ⊕ Sz1
-space = 2
+  # Make a random network
+  max_nedges = max(1, N * (N-1) ÷ 2)
+  # Fill 2/3 of the edges
+  nedges = max(1, max_nedges * 2 ÷ 3)
+  # 1/3 of tensors have physical indices
+  nsites = N ÷ 3
+  edges = StatsBase.sample(collect(subsets(1:N, 2)), nedges; replace = false)
+  sites = StatsBase.sample(collect(1:N), nsites; replace = false)
 
-# Make a random network
-max_nedges = max(1, N * (N-1) ÷ 2)
-# Fill 2/3 of the edges
-nedges = max(1, max_nedges * 2 ÷ 3)
-# 1/3 of tensors have physical indices
-nsites = N ÷ 3
-edges = StatsBase.sample(collect(subsets(1:N, 2)), nedges; replace = false)
-sites = StatsBase.sample(collect(1:N), nsites; replace = false)
+  indsnetwork = ITensorsInfiniteMPS.IndexSetNetwork(N)
+  for e in edges
+    le = IndexSet(Index(space, "l=$(e[1])↔$(e[2])"))
+    pair_e = Pair(e...)
+    indsnetwork[pair_e] = le
+    indsnetwork[reverse(pair_e)] = dag(le)
+  end
 
-indsnetwork = ITensorsInfiniteMPS.IndexSetNetwork(N)
-for e in edges
-  le = IndexSet(Index(space, "l=$(e[1])↔$(e[2])"))
-  pair_e = Pair(e...)
-  indsnetwork[pair_e] = le
-  indsnetwork[reverse(pair_e)] = dag(le)
+  for n in sites
+    indsnetwork[n => n] = IndexSet(Index(space, "s=$(n)"))
+  end
+
+  T = Vector{ITensor}(undef, N)
+  for n in 1:N
+    T[n] = randomITensor(only.(ITensorsInfiniteMPS.eachlinkinds(indsnetwork, n))...)
+  end
+
+  # Can just use indsnetwork, this recreates indsnetwork
+  TN = ⊗(T...)
+
+  #@show N
+
+  #sequence, cost = @time ITensorsInfiniteMPS.optimal_contraction_sequence(TN)
+  #@show cost
+  #@show sequence
+  #@show Tree(sequence)
+  #@profview ITensorsInfiniteMPS.optimal_contraction_sequence(TN)
+
+  stats = @timed depth_first_constructive(T; enable_caching = enable_caching)
+  sequence2, cost2 = stats.value
+  time = stats.time
+  #@show cost2
+  #@show sequence2
+  #@show Tree(sequence2)
+  println(N, "  ", time, "  ", cost2, "  ", sequence2)
+  #@profview depth_first_constructive(T)
 end
 
-for n in sites
-  indsnetwork[n => n] = IndexSet(Index(space, "s=$(n)"))
-end
-
-T = Vector{ITensor}(undef, N)
-for n in 1:N
-  T[n] = randomITensor(only.(ITensorsInfiniteMPS.eachlinkinds(indsnetwork, n))...)
-end
-
-# Can just use indsnetwork, this recreates indsnetwork
-TN = ⊗(T...)
-
-@show N
-
-#sequence, cost = @time ITensorsInfiniteMPS.optimal_contraction_sequence(TN)
-#@show cost
-#@show sequence
-#@show Tree(sequence)
-#@profview ITensorsInfiniteMPS.optimal_contraction_sequence(TN)
-
-stats = @timed depth_first_constructive(T)
-sequence2, cost2 = stats.value
-time = stats.time
-@show cost2
-@show sequence2
-@show Tree(sequence2)
-println(N, "  ", time, "  ", cost2, "  ", sequence2)
-#@profview depth_first_constructive(T)
-
+#
+# 62cd05c58dffcd278bb87b7df10e616357a0e2f4
+#
+# XXX: enable_caching = false for N < 7
+#
+# enable_caching = false
+#
+# 2  1.027e-6      0      Any[1 2]
+# 3  6.3711e-5     8      Any[3, Any[1, 2]]
+# 4  5.3855e-5     24     Any[4, Any[1, Any[2, 3]]]
+# 5  8.1188e-5     48     Any[5, Any[1, Any[4, Any[2, 3]]]]
+# 6  0.000532094   384    Any[2, Any[Any[1, Any[3, 6]], Any[4, 5]]]
+# 7  0.015745355   1744   Any[3, Any[7, Any[1, Any[5, Any[6, Any[2, 4]]]]]]
+# 8  0.208512772   4928   Any[7, Any[Any[2, 3], Any[8, Any[5, Any[4, Any[1, 6]]]]]]
+# 9  12.773462454  82432  Any[2, Any[6, Any[8, Any[3, Any[7, Any[5, Any[9, Any[1, 4]]]]]]]]
+#
+# enable_caching = true
+#
+# 2  7.63e-7      0      Any[1 2]
+# 3  7.3079e-5    8      Any[3, Any[1, 2]]
+# 4  6.2208e-5    24     Any[4, Any[1, Any[2, 3]]]
+# 5  0.000106835  48     Any[5, Any[1, Any[4, Any[2, 3]]]]
+# 6  0.000711473  384    Any[2, Any[Any[1, Any[3, 6]], Any[4, 5]]]
+# 7  0.006858842  1744   Any[3, Any[7, Any[1, Any[5, Any[6, Any[2, 4]]]]]]
+# 8  0.068384588  4928   Any[7, Any[Any[2, 3], Any[8, Any[5, Any[4, Any[1, 6]]]]]]
+# 9  4.583904197  82432  Any[2, Any[6, Any[8, Any[3, Any[7, Any[5, Any[9, Any[1, 4]]]]]]]]
+#
+#
+# bf5885d2d7234d9a59a38397d5acdb3eef9333a7
+#
+# N  time(s)     cost  sequence
+# 2  5.5955e-5   2     Any[1, 2]
+# 3  7.2351e-5   8     Any[3, Any[1, 2]]
+# 4  9.5568e-5   24    Any[4, Any[1, Any[2, 3]]]
+# 5  0.000440426 48    Any[5, Any[1, Any[4, Any[2, 3]]]]
+# 6  0.002089596 384   Any[2, Any[Any[1, Any[3, 6]], Any[4, 5]]]
+# 7  0.008770299 1744  Any[3, Any[7, Any[1, Any[5, Any[6, Any[2, 4]]]]]]
+# 8  0.069674617 4928  Any[7, Any[Any[2, 3], Any[8, Any[5, Any[4, Any[1, 6]]]]]]
+# 9  4.641397023 82432 Any[2, Any[6, Any[8, Any[3, Any[7, Any[5, Any[9, Any[1, 4]]]]]]]]
+#
+#
 #
 # de8fc59563ad546b1f0b6be1af130a260e3f67d5
 #
---
+++ examples/profile.jl
@@ -0,0 +1,82 @@
+using ITensors
+using ITensorsInfiniteMPS
+using ITensorsInfiniteMPS.ContractionSequenceOptimization
+using ProfileView
+
+function main(N, d = 2; random_order = false)
+  @show N
+  @show d
+
+  i = Index(d, "i")
+  A = randomITensor(i', dag(i))
+
+  function A⃗(N; random_order = false)
+    v = [prime(A, n) for n in reverse(0:N-1)]
+    if random_order
+      return shuffle(v)
+    end
+    return v
+  end
+  f(A, N; kwargs...) = for _ in 1:N depth_first_constructive(A; kwargs...) end
+
+  AN = A⃗(N; random_order = random_order)
+  display(inds.(AN))
+  println()
+
+  println("Sequence optimization time, no caching")
+  enable_caching = false
+  @show enable_caching
+  @show depth_first_constructive(AN; enable_caching = enable_caching)
+  @btime depth_first_constructive($AN; enable_caching = $enable_caching)
+
+  println()
+  println("Sequence optimization time, caching")
+  enable_caching = true
+  @show enable_caching
+  @show depth_first_constructive(AN; enable_caching = enable_caching)
+  @btime depth_first_constructive($AN; enable_caching = $enable_caching)
+
+  if !random_order
+    println()
+    println("Contraction time")
+    @btime *($AN...)
+    #@profview f(AN, 1e6; enable_caching = enable_caching)
+  end
+
+  return
+end
+
+#
+# Results
+#
+# 62cd05c58dffcd278bb87b7df10e616357a0e2f4
+#
+# d = 2, matrix multiplications
+#
+# Starting from optimal ordering
+#
+# N  No caching    Caching      Contraction time
+# 2  37.923 ns     38.069 ns    854.769 ns
+# 3  1.835 μs      3.812 μs     1.663 μs
+# 4  5.878 μs      13.506 μs    2.462 μs
+# 5  30.853 μs     67.228 μs    4.128 μs
+# 6  221.736 μs    424.022 μs   5.276 μs
+# 7  1.962 ms      3.264 ms     6.156 μs
+# 8  29.596 ms     31.897 ms    7.008 μs
+# 9  392.239 ms    375.781 ms   8.094 μs
+# 10 5.210 s       4.807 s      8.163 μs
+#
+# Random start ordering
+#
+# N  No caching    Caching
+# 2  38.701 ns     38.693 ns
+# 3  2.311 μs      4.630 μs
+# 4  6.179 μs      14.231 μs
+# 5  30.074 μs     67.212 μs
+# 6  237.478 μs    445.363 μs
+# 7  2.060 ms      3.354 ms
+# 8  33.332 ms     35.449 ms
+# 9  475.974 ms    431.469 ms
+# 10 6.759 s       5.740 s
+#
+
