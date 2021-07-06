6192f3a0abb575f32b57aa7147e292ec6c183985
Matthew Fishman <mfishman@flatironinstitute.org>
2021-07-05 22:22:07 -0400

    Add CelledVector as data storage type for InfiniteMPS

--- examples/finite_mps_to_infinite_mps.jl
+++ examples/finite_mps_to_infinite_mps.jl
@@ -1,6 +1,9 @@
 using ITensors
 using ITensorInfiniteMPS
 
+#InfiniteSumLocalOps <: AbstractMPS
+
+# H = -J Σⱼ XⱼXⱼ₊₁ - h Σⱼ Zⱼ
 function ising_mpo(s; J, h)
   N = length(s)
   a = AutoMPO()
@@ -13,8 +16,58 @@ function ising_mpo(s; J, h)
   H = MPO(a, s)
 end
 
-function ising_local_ham(s1::Index, s2::Index; J, h)
+function ising_infinitempo_tensor(s⃗, l, r; J, h)
+  @show s⃗
+  s = only(s⃗)
+  #h = emptyITensor(s', dag(s), l, r)
+  dₗ = 3 # The link dimension of the TFI
+  Hmat = fill(ITensor(s', dag(s)), dₗ, dₗ)
+  Hmat[1, 1] = op("Id", s)
+  Hmat[2, 2] = op("Id", s)
+  Hmat[3, 3] = op("Id", s)
+  Hmat[2, 1] = -J * op("X", s)
+  Hmat[3, 2] = -J * op("X", s)
+  Hmat[3, 1] = -h * op("Z", s)
+  H = emptyITensor()
+  for i in 1:dₗ, j in 1:dₗ
+    H += Hmat[i,j] * setelt(l => i) * setelt(r => j)
+  end
+  return H
+end
+
+# H = -J Σⱼ XⱼXⱼ₊₁ - h Σⱼ Zⱼ
+function ising_infinitempo(s; J, h)
+  N = length(s)
+  # Use a finite MPO to create the infinite MPO
+  H = InfiniteMPO(N)
+  # For the finite unit cell
+  s¹ = addtags.(s, "c=1")
+  l¹ = [Index(3, "Link,l=$n,c=1") for n in 1:N]
+  for n in 1:N
+    # TODO: use a CelledVector here to make the code logic simpler
+    l¹ₙ = n == 1 ? replacetags(dag(l¹[N]), "c=1" => "c=0") : l¹[n-1]
+    r¹ₙ = l¹[n]
+    H[n] = ising_infinitempo_tensor(s¹[n], l¹ₙ, r¹ₙ; J = J, h = h)
+  end
+  return H
+end
+
+# H = -J X₁X₂ - h Z₁
+function ising_localop(s1::Index, s2::Index; J, h)
   return -J * op("X", s1) * op("X", s2) - h * op("Z", s1) * op("Id", s2)
+end
+# Version accepting IndexSet
+ising_localop(s1, s2; kwargs...) = ising_localop(Index(s1), Index(s2); kwargs...)
+
+function ising_infinitesumlocalops(s; J, h)
+  N = length(s)
+  H = InfiniteSumLocalOps(N)
+  @show s[1]
+  s∞ = CelledVector(s)
+  @show s∞[1]
+  @show s∞[2]
+  @show s∞[3]
+  return InfiniteSumLocalOps([ising_localop(s∞[n], s∞[n+1]; J = J, h = h) for n in 1:N])
 end
 
 let
@@ -40,14 +93,14 @@ let
   println("\nFinite MPS energy on bond (n1, n2) = $((n1, n2))")
 
   ϕ12 = ψ[n1] * ψ[n2]
-  ham12 = ising_local_ham(s[n1], s[n2]; J = J, h = h)
+  ham12 = ising_localop(s[n1], s[n2]; J = J, h = h)
   @show (ϕ12 * ham12 * prime(dag(ϕ12), commoninds(ϕ12, ham12)))[]
 
   # Get an infinite MPS that approximates
   # the finite MPS in the bulk.
   # `nsites` sets the number of sites in the unit
   # cell of the infinite MPS.
-  nsites = 2
+  nsites = 1
   ψ∞ = infinitemps_approx(ψ; nsites = nsites, nsweeps = 10)
 
   println("\nInfinite MPS energy on a few different bonds")
@@ -60,9 +113,35 @@ let
     ϕ12 = ψ∞.AL[n1] * ψ∞.AL[n2] * ψ∞.C[n2]
     s1 = siteind(ψ∞, n1)
     s2 = siteind(ψ∞, n2)
-    ham12 = ising_local_ham(s1, s2; J = J, h = h)
+    ham12 = ising_localop(s1, s2; J = J, h = h)
     ϕ12ᴴ = prime(dag(ϕ12), commoninds(ϕ12, ham12))
     @show (ϕ12 * ham12 * ϕ12ᴴ)[] / norm(ϕ12)
   end
+
+#  s¹ = siteinds(ψ∞, Cell(1))
+#
+#  # Test computing the energy from an InfiniteMPO
+#  H∞ = ising_infinitempo(s¹; J = J, h = h)
+#  L = ITensorInfiniteMPS.left_environment(H∞, ψ∞.AL, ψ∞.C)
+#  R = ITensorInfiniteMPS.right_environment(H∞, ψ∞.AR, ψ∞.C)
+#  ϕ0 = ψ∞.C[1]
+#  Hϕ0 = noprime(L[1] * ϕ0 * R[1])
+#  @show (dag(ϕ0) * Hϕ0)[]
+#
+#  ϕ1 = ψ∞.AL[1] * ψ∞.C[1]
+#  Hϕ1 = noprime(L[0] * ϕ1 * H∞[1] * R[1])
+#  @show (dag(ϕ1) * Hϕ1)[]
+#  @show norm(ϕ1)
+#  @show norm(ϕ0)
+#
+#  # Test computing the expectation value of an infinite sum
+#  # of local operators
+#  # InfiniteSumLocalOps
+#  Σ∞h = ising_infinitesumlocalops(s¹; J = J, h = h)
+#  ψ12 = ψ∞.AL[1] * ψ∞.AL[2]
+#  h12 = Σ∞h[1]
+#  s12 = commoninds(ψ12, h12)
+#  @show ψ12 * h12 * prime(dag(ψ12), s12)
+
 end
 
--- src/infinitempo.jl
+++ src/infinitempo.jl
@@ -10,3 +10,10 @@ mutable struct InfiniteMPO <: AbstractInfiniteMPS
   reverse::Bool
 end
 
+struct InfiniteSumLocalOps <: AbstractInfiniteMPS
+  data::CelledVector{ITensor}
+end
+InfiniteSumLocalOps(N::Int) = InfiniteSumLocalOps(Vector{ITensor}(undef, N))
+InfiniteSumLocalOps(data::Vector{ITensor}) = InfiniteSumLocalOps(CelledVector(data))
+getindex(l::InfiniteSumLocalOps, n::Integer) = ITensors.data(l)[n]
+
