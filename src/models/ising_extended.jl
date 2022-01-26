c298a96d0178e80a49d3640e4bb279b0d80e8699
LHerviou <loic.herviou@epfl.ch>
2022-01-26 17:13:27 +0100

    Fixed the old models ising and xx such that they work with the OpSum formalism, added xx_extended for further benchmark, fixed formatting

--- src/models/ising_extended.jl
+++ src/models/ising_extended.jl
@@ -1,6 +1,6 @@
 nrange(::Model"ising_extended") = 3
 # H = -J Σⱼ XⱼXⱼ₊₁ - h Σⱼ Zⱼ - J₂ Σⱼ XⱼZⱼ₊₁Xⱼ₊₂
-function ITensors.MPO(::Model"ising_extended", s; J = 1.0, h = 1.0, J₂ = 0.0)
+function ITensors.MPO(::Model"ising_extended", s; J=1.0, h=1.0, J₂=0.0)
   N = length(s)
   a = OpSum()
   if J != 0
@@ -25,20 +25,19 @@ end
 function ITensors.OpSum(::Model"ising_extended", n1, n2; J=1.0, h=1.0, J₂=0.0)
   opsum = OpSum()
   if J != 0
-    opsum += -J/2, "X", n1, "X", n2
-    opsum += -J/2, "X", n2, "X", n2+1
+    opsum += -J / 2, "X", n1, "X", n2
+    opsum += -J / 2, "X", n2, "X", n2 + 1
   end
   if J₂ != 0
-    opsum += -J₂, "X", n1, "Z", n2, "X", n2+1
+    opsum += -J₂, "X", n1, "Z", n2, "X", n2 + 1
   end
-  opsum += -h/3, "Z", n1
-  opsum += -h/3, "Z", n2
-  opsum += -h/3, "Z", n2+1
+  opsum += -h / 3, "Z", n1
+  opsum += -h / 3, "Z", n2
+  opsum += -h / 3, "Z", n2 + 1
   return opsum
 end
 
-
 function reference(::Model"ising_extended", ::Observable"energy"; J=1.0, h=1.0, J₂=0.0)
-  f(k) = sqrt( ( J * cos(k) + J₂ * cos(2k) - h )^2 + ( J * sin(k) + J₂ * sin(2k))^2 )
-  return - 1 / 2π * ITensorInfiniteMPS.∫(k -> f(k), -π, π)
+  f(k) = sqrt((J * cos(k) + J₂ * cos(2k) - h)^2 + (J * sin(k) + J₂ * sin(2k))^2)
+  return -1 / 2π * ITensorInfiniteMPS.∫(k -> f(k), -π, π)
 end
