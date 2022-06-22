55512501557997f5c38107578ed84092a29708ce
Matthew Fishman <mfishman@flatironinstitute.org>
2022-06-22 19:29:08 -0400

    Replace broken subtraction in OpSum with addition

--- src/models/ising_extended.jl
+++ src/models/ising_extended.jl
@@ -3,9 +3,9 @@
 # H = -J Σⱼ XⱼXⱼ₊₁ - h Σⱼ Zⱼ - J₂ Σⱼ XⱼZⱼ₊₁Xⱼ₊₂
 function unit_cell_terms(::Model"ising_extended"; J=1.0, h=1.0, J₂=0.0)
   opsum = OpSum()
-  opsum -= J, "X", 1, "X", 2
-  opsum -= J₂, "X", 1, "Z", 2, "X", 3
-  opsum -= h, "Z", 1
+  opsum += -J, "X", 1, "X", 2
+  opsum += -J₂, "X", 1, "Z", 2, "X", 3
+  opsum += -h, "Z", 1
   return [opsum]
 end
 
