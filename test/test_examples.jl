f80abc25e8d8fa6dcd1e59b30e06fe34a6fd72b5
Matthew Fishman <mfishman@flatironinstitute.org>
2022-06-22 20:48:21 -0400

    Fix path in examples

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -1,8 +1,9 @@
 using Test
 using Suppressor
 
+examples_dir = joinpath(@__DIR__, "..", "examples", "vumps")
 @testset "examples" begin
-  @testset "$file" for file in joinpath(readdir(@__DIR__), "..", "examples", "vumps")
+  @testset "$file" for file in readdir(examples_dir; join=true)
     if endswith(file, ".jl")
       println("Running $file")
       @suppress include(file)
