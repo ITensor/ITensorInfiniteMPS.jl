56de067ba36f7c415f55d53f4ba808cd87b85fc7
Jan Reimers <janreimers@gmail.com>
2023-04-01 10:09:15 -0600

    FInish cleanup of MPOMatrix-->InfiniteMPO conversion

--- src/infinitempomatrix.jl
+++ src/infinitempomatrix.jl
@@ -7,6 +7,7 @@ mutable struct InfiniteMPOMatrix <: AbstractInfiniteMPS
 end
 
 translator(mpo::InfiniteMPOMatrix) = mpo.data.translator
+data(mpo::InfiniteMPOMatrix) = mpo.data
 
 # TODO better printing?
 function Base.show(io::IO, M::InfiniteMPOMatrix)
@@ -53,10 +54,12 @@ end
 #    0         0          ...  l=2-->M-->l=1     0      0
 #    0         0          ...        0        l=1-->M   I
 #
-# We need to capture the corner I's and the sub-diagonal Ms, and paste them together into on ITensor.
-# In addition we need make all elements of Hm into order(4) tensors by adding dummy Dw=1 indices.
+#  We need to capture the corner I's and the sub-diagonal Ms, and paste them together into an ITensor.
+#  This is facilutated by making all elements of Hm into order(4) tensors by adding dummy Dw=1 indices.
+#  We ITensors.directsum() to join all the blocks into the final ITensor.
+#  This code should be 100% dense/blocks-sparse agnostic.
 #
-function matrixITensorToITensor(Hm::Matrix{ITensor})
+function matrixITensorToITensor(Hm::Matrix{ITensor})::ITensor
   indexT=typeof(inds(Hm[1,1])[1])
   T=eltype(Hm[1,1])
   lx, ly = size(Hm)
@@ -65,18 +68,15 @@ function matrixITensorToITensor(Hm::Matrix{ITensor})
   #  Extract the sub diagonal
   #
   Ms=map(n->Hm[lx-n+1,ly-n],1:lx-1) #Get the sub diagonal into an array.
-  #Ms=map(n->H[n+1,n],1:lx-1) #Get the sub diagonal into an array, reverse order.
   N=length(Ms)
   @assert N==lx-1
   #
   #  We need a dummy link index, but it has to have the right direction.  
-  #  Ms[1] should only have one link index, and it should be to the right.
+  #  Ms[1] should only have one link index which should be pointing to the right.
   #
   @assert length(inds(Ms[1],tags="Link"))==1
   ir,=inds(Ms[1],tags="Link")
-  left_dir=dir(dag(ir))
-  tspace=ITensors.trivial_space(ir)
-  il0=Index(tspace;dir=left_dir,tags="Link,l=0") #Dw=1 left dummy index.
+  il0=Index(ITensors.trivial_space(ir);dir=dir(dag(ir)),tags="Link,l=0") #Dw=1 left dummy index.
   #
   # Convert edge Ms to order 4 ITensors using the dummy index.
   #
@@ -87,13 +87,12 @@ function matrixITensorToITensor(Hm::Matrix{ITensor})
   #  can't use QN directions to decide left/right.  Instead we look for common tags among the neighbouring Ms.
   #  Right now the code assumes plev=0 for all Ms.
   #
-  ils,irs=indexT[],indexT[] #left the right link indices.
-  ir=il0 #set up recursion below.  ir from the previous M, should have the same tags as il on the next M.
+  is=NTuple{2,indexT}[]
+  ir=il0 #set up recursion below.  ir is from the previous M, should have the same tags as il on the next M.
   for n in 1:N
     il,=inds(Ms[n],tags=tags(ir)) #Find the left index
-    ir=noncommonind(Ms[n],il,tags="Link") #New right index by elimination
-    push!(ils,il)
-    push!(irs,ir)      
+    ir=noncommonind(Ms[n],il,tags="Link") #Find the new right index by elimination
+    push!(is,(il,ir))    
   end 
   #
   # Now direct sum everything non-empty in Hm, including those I ops in the corners.
@@ -101,35 +100,35 @@ function matrixITensorToITensor(Hm::Matrix{ITensor})
   # Details of the Link index tags like "l=n" do not matter from here on.
   #
   H=Hm[1,1]*onehot(T, il0' => 1)*onehot(T, dag(il0) => 1) #Bootstrap with the I op in the top left of Hm
-  H,ih=directsum(H=>il0',Ms[N]=>ils[N]) #1-D directsum to put M[N] directly below the I op
+  H,ih=directsum(H=>il0',Ms[N]=>is[N][1]) #1-D directsum to put M[N] directly below the I op
   ih=ih,dag(il0) #setup recusion.
   for i in N-1:-1:1 #2-D directsums to place new blocks below and to the right.
-      H,ih=directsum(H=>ih,Ms[i]=>(ils[i],irs[i]),tags=["Link","Link"]) 
+      H,ih=directsum(H=>ih,Ms[i]=>is[i],tags=["Link,left","Link,right"]) 
   end
   IN=Hm[N+1,N+1]*onehot(T, dag(il0) => 1)*onehot(T, ih[1] => dim(ih[1])) #I op in the bottom left of Hm
-  H,_=directsum(H=>ih[2],IN=>il0,tags="Link") #1-D directsum to the put I in the bottom row, to the right of M[1]
-
-  ih=inds(H,tags="Link")
-  return H,ih[1],ih[2]
+  H,_=directsum(H=>ih[2],IN=>il0,tags="Link,right") #1-D directsum to the put I in the bottom row, to the right of M[1]
+  return H
 end
-
-function InfiniteMPO(H::InfiniteMPOMatrix)
-  temp = matrixITensorToITensor.(H)
-  new_H = CelledVector([x[1] for x in temp], translator(H))
-  lis = CelledVector([x[2] for x in temp], translator(H))
-  ris = CelledVector([x[3] for x in temp], translator(H))
-  #retags the right_links
-  s = [commoninds(H[j][1, 1], H[j][end, end])[1] for j in 1:nsites(H)]
-  for j in 1:nsites(H)
-    newTag = "Link,c=$(getcell(s[j])),l=$(getsite(s[j]))"
-    temp = replacetags(ris[j], tags(ris[j]), newTag)
-    new_H[j] = replaceinds(new_H[j], [ris[j]], [temp])
-    ris[j] = replacetags(ris[j], tags(ris[j]), newTag)
+#
+#  Hm is the InfiniteMPOMatrix
+#  Hs is an array of ITensors, one for the site in the unit cell.
+#  Hi is a CelledVector of ITensors.
+#
+function InfiniteMPO(Hm::InfiniteMPOMatrix)
+  Hs = matrixITensorToITensor.(data(Hm))
+  Hi = CelledVector([H for H in Hs], translator(Hm))
+  lis = CelledVector([inds(H,tags="left")[1] for H in Hs], translator(Hm))
+  ris = CelledVector([inds(H,tags="right")[1] for H in Hs], translator(Hm))
+  site_inds = [commoninds(Hm[j][1, 1], Hm[j][end, end])[1] for j in 1:nsites(Hm)]
+  #
+  #  Create new tags with proper cell and link numbers.  Also daisy chain
+  #  all the indices so right index at j = dag(left index at j+1)
+  #
+  for j in 1:nsites(Hm)
+    newTag = "Link,c=$(getcell(site_inds[j])),l=$(getsite(site_inds[j]))"
+    ir = replacetags(ris[j], tags(ris[j]), newTag) #new right index
+    Hi[j] = replaceinds(Hi[j], [ris[j]], [ir])
+    Hi[j + 1] = replaceinds(Hi[j + 1],[lis[j + 1]],[dag(ir)])
   end
-  # joining the indexes
-  for j in 1:nsites(H)
-    temp = δ(dag(ris[j]), dag(lis[j + 1]))
-    new_H[j + 1] *= temp
-  end
-  return InfiniteMPO(new_H)
+  return InfiniteMPO(Hi)
 end
